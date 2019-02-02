cda4525890d07ab4af35bb4562c6bdc3978940ae
Sarjeraoss <30568261+Sarjeraoss@users.noreply.github.com>
2019-02-02 13:08:17 +0530

    Update conf.php
    Submit

--- services/conf.php
+++ services/conf.php
@@ -6,25 +6,25 @@ class CONF {
     public $DEMO_VERSION = false;
 
     /* Data configuration for database */
-    public $DB_SERVER   = "localhost";
-    public $DB_USER     = "root";
-    public $DB_PASSWORD = "";
-    public $DB_NAME     = "markeet";
+    public $DB_SERVER   = "sql12.freesqldatabase.com";
+    public $DB_USER     = "sql12276976";
+    public $DB_PASSWORD = "NXf428UDck";
+    public $DB_NAME     = "sql12276976";
 
     /* FCM key for notification */
-    public $FCM_KEY     = "AIzaSyCv-90mFpx3SCWlIKSXXXXXXXXXXXXXXXXX";
+    public $FCM_KEY     = "AIzaSyB9WsuT3SlssJVjAlfXd0d6QliK6mlMsDs";
 
 
     /* [ IMPORTANT ] be careful when edit this security code, use AlphaNumeric only*/
     /* This string must be same with security code at Android, if its different android unable to submit order */
-    public $SECURITY_CODE = "8V06LupAaMBLtQqyqTxmcN42nn27FlejvaoSM3zXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX";
+    public $SECURITY_CODE = "DjB9E1ing2HHvQNDtBlYxdl85iNDI6FxagYziqXOiQ4ykjrSg9CtqZCPKOY3N5XtUJ2zYnpYUWA0wWN1B9HhW5IbqZyMr5enwMaC";
 
     /* Mailer config ---------------------------------------------------- */
 
     // change with yours
-    public $SMTP_EMAIL      = "sample@your-domain.com";
-    public $SMTP_PASSWORD   = "password";
-    public $SMTP_HOST       = "mail.your-domain.com";
+    public $SMTP_EMAIL      = "sample@peygreatz.com";
+    public $SMTP_PASSWORD   = "testing";
+    public $SMTP_HOST       = "mail.peygreatz.com";
     public $SMTP_PORT       = 562;
 
     // for administrator & for buyer
